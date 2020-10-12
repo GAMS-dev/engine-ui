e904437fdb62d38bff21f184bfa8839912714e96
Frederik Proske <fproske@gams.com>
2020-10-12 16:48:10 +0200

    Implement option to force-kill a job when soft kill doesn't work

--- src/components/JobActionsButtonGroup.jsx
+++ src/components/JobActionsButtonGroup.jsx
@@ -1,24 +1,24 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import DownloadLink from "./DownloadLink";
-import {AlertContext} from "./Alert";
+import { AlertContext } from "./Alert";
 import axios from "axios";
 
 const JobActionsButtonGroup = props => {
   const { token, status, server, setRefresh } = props;
-  
+
   const [, setAlertMsg] = useContext(AlertContext);
 
-  function terminateJob() {
+  function terminateJob(e, hardKill = false) {
     axios
       .delete(
-        token.startsWith("hc:")? `${server}/hypercube/${token.substring(3)}`: `${server}/jobs/${token}`,
+        token.startsWith("hc:") ? `${server}/hypercube/${token.substring(3)}?hard_kill=${hardKill}` : `${server}/jobs/${token}?hard_kill=${hardKill}`,
         {}
       )
       .then(res => {
         setRefresh(refreshCnt => ({
           refresh: refreshCnt + 1
-       }));
+        }));
       })
       .catch(err => {
         setAlertMsg(`Problems terminating job. Error message: ${err.message}`);
@@ -30,15 +30,17 @@ const JobActionsButtonGroup = props => {
       <Link to={"/jobs/" + token} className="btn btn-sm btn-outline-info">
         Show
       </Link>
-      {status > 9 && 
-      <DownloadLink 
-        url={token.startsWith("hc:")? `${server}/hypercube/${token.substring(3)}/result`: `${server}/jobs/${token}/result`} 
-        filename="results.zip" 
-        className="btn btn-sm btn-outline-info">
-        Download
+      {status > 9 &&
+        <DownloadLink
+          url={token.startsWith("hc:") ? `${server}/hypercube/${token.substring(3)}/result` : `${server}/jobs/${token}/result`}
+          filename="results.zip"
+          className="btn btn-sm btn-outline-info">
+          Download
       </DownloadLink>}
       {status >= 0 && status < 10 &&
-      <button className="btn btn-sm btn-outline-danger" onClick={terminateJob}>Cancel</button>}
+        <button className="btn btn-sm btn-outline-danger" onClick={terminateJob}>Cancel</button>}
+      {status === -2 &&
+        <button className="btn btn-sm btn-outline-danger" onClick={e => terminateJob(e, true)}>Hard kill</button>}
     </div>
   );
 };
