f3f2ce0dab053ee67d584d31000e2858277a14b9
hbusul <h.burak.usul@gmail.com>
2021-01-15 22:11:51 +0300

    put default values for ENGINE_URL and BASE_URL and do not override entry point instead use /docker-entrypoint.d

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,5 @@ RUN npm run build
 
 FROM nginx:latest
 COPY --from=builder /app/build /usr/share/nginx/engine
-COPY boot.sh .
-RUN chmod +x boot.sh
-ENTRYPOINT ["./boot.sh"]
+COPY boot.sh /docker-entrypoint.d/30-fix-ui-vars.sh
+RUN chmod +x /docker-entrypoint.d/30-fix-ui-vars.sh
--- boot.sh
+++ boot.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+if [ -z ${ENGINE_URL+x} ]
+then
+    echo "ENGINE_URL is unset, using default /api"
+    export ENGINE_URL=/api
+fi
+
+if [ -z ${BASE_URL+x} ]
+then
+    echo "BASE_URL is unset, using default /"
+    export BASE_URL=/
+fi
+
 ENGINE_URL_ESCAPED=$(echo ${ENGINE_URL} | sed -e 's/[&/\]/\\&/g')
 BASE_URL_ESCAPED=$(echo ${BASE_URL} | sed -e 's/[&/\]/\\&/g')
-find . -type f -name 'main*.chunk.js*' | xargs sed -i "s/AAAABBBBCCCC/${ENGINE_URL_ESCAPED}/g"
-find . -type f -name 'main*.chunk.js*' | xargs sed -i "s/DDDDEEEEFFFF/${BASE_URL_ESCAPED}/g"
-exec nginx -g "daemon off;"
+find /usr/share/nginx/engine -type f -name 'main*.chunk.js*' | xargs sed -i "s/AAAABBBBCCCC/${ENGINE_URL_ESCAPED}/g"
+find /usr/share/nginx/engine -type f -name 'main*.chunk.js*' | xargs sed -i "s/DDDDEEEEFFFF/${BASE_URL_ESCAPED}/g"
