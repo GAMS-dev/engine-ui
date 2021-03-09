cc549189be49571b84f20894c738df8b045c7c9a
Frederik Proske <fproske@gams.com>
2021-03-09 19:23:00 +0100

    Replace special strings in all files not just main.js

--- boot.sh
+++ boot.sh
@@ -13,5 +13,5 @@ fi
 
 ENGINE_URL_ESCAPED=$(echo ${ENGINE_URL} | sed -e 's/[&/\]/\\&/g')
 BASE_URL_ESCAPED=$(echo ${BASE_URL} | sed -e 's/[&/\]/\\&/g')
-find /usr/share/nginx/engine -type f -name 'main*.chunk.js*' | xargs sed -i "s/AAAABBBBCCCC/${ENGINE_URL_ESCAPED}/g"
-find /usr/share/nginx/engine -type f -name 'main*.chunk.js*' | xargs sed -i "s/DDDDEEEEFFFF/${BASE_URL_ESCAPED}/g"
+grep -lr "AAAABBBBCCCC" /usr/share/nginx/engine | xargs sed -i "s/AAAABBBBCCCC/${ENGINE_URL_ESCAPED}/g"
+grep -lr "DDDDEEEEFFFF" /usr/share/nginx/engine | xargs sed -i "s/DDDDEEEEFFFF/${BASE_URL_ESCAPED}/g"
