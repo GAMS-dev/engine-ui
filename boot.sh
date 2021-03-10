160da8f05c265abcdcd4c821ba263d86c80cec40
Frederik Proske <fproske@gams.com>
2021-03-10 12:51:11 +0100

    Fix booth.sh

--- boot.sh
+++ boot.sh
@@ -13,7 +13,7 @@ fi
 
 ENGINE_URL_ESCAPED=$(echo ${ENGINE_URL} | sed -e 's/[&/\]/\\&/g')
 BASE_URL_ESCAPED=$(echo ${BASE_URL} | sed -e 's/[&/\]/\\&/g')
-BASE_URL_ESCAPED_STRIPPED=$(echo ${BASE_URL_ESCAPED} | sed -e 's/\/$//g')
+BASE_URL_ESCAPED_STRIPPED=$(echo ${BASE_URL_ESCAPED} | sed -e 's/\\/$//g')
 find /usr/share/nginx/engine -type f -name 'main*.chunk.js*' | xargs sed -i "s/AAAABBBBCCCC/${ENGINE_URL_ESCAPED}/g"
 find /usr/share/nginx/engine -type f -name 'main*.chunk.js*' | xargs sed -i "s/DDDDEEEEFFFF/${BASE_URL_ESCAPED}/g"
 grep -lr "GGGGHHHHIIIIJJJJ" /usr/share/nginx/engine | xargs sed -i "s/GGGGHHHHIIIIJJJJ/${BASE_URL_ESCAPED_STRIPPED}/g"
