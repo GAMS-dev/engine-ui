5cb34a91a1cd9e42a1c99d20e5cf32641a8ca790
Frederik Proske <fproske@gams.com>
2021-06-25 12:46:26 +0200

    Allow inviting users with quotas, groups and instances
    The UserInvitationForm component now allow specifying:
    - user groups per namespace
    - instances
    - quotas

--- src/components/UserInvitationForm.jsx
+++ src/components/UserInvitationForm.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../AuthContext";
 import axios from "axios";
+import Select from 'react-select';
 import { getResponseError } from "./util";
 import NamespacePermissionSelector from "./NamespacePermissionSelector";
 import SubmitButton from "./SubmitButton";
 import ClipLoader from "react-spinners/ClipLoader";
+import UserQuotaSelector from "./UserQuotaSelector";
 
 const UserInvitationForm = () => {
     const [{ jwt, server, roles, username }] = useContext(AuthContext);
@@ -14,32 +16,74 @@ const UserInvitationForm = () => {
     const [copySuccessMsg, setCopySuccessMsg] = useState("");
     const [role, setRole] = useState("user");
     const [namespacePermissions, setNamespacePermissions] = useState([]);
+    const [assignInstances, setAssignInstances] = useState(false);
+    const [availableInstances, setAvailableInstances] = useState([]);
+    const [selectedInstancesAllowed, setSelectedInstancesAllowed] = useState(null);
+    const [defaultInstance, setDefaultInstance] = useState(null);
+    const [assignQuotas, setAssignQuotas] = useState(false);
+    const [quotas, setQuotas] = useState(null);
     const [isSubmitting, setIsSubmitting] = useState(false);
 
     const [invitationCode, setInvitationCode] = useState("");
 
     useEffect(() => {
-        axios
-            .get(`${server}/namespaces/`, {
-                headers: { "X-Fields": "name,permissions" }
-            })
-            .then(res => {
-                if (res.status !== 200) {
-                    setSubmissionErrorMsg("An error occurred while retrieving namespaces. Please try again later.");
-                    setIsLoading(false);
-                    return;
+        const fetchRequiredData = async () => {
+            const requests = [
+                axios
+                    .get(`${server}/namespaces/`, {
+                        headers: { "X-Fields": "name,permissions" }
+                    })
+                    .then(res => {
+                        if (res.status !== 200) {
+                            setSubmissionErrorMsg("An error occurred while retrieving namespaces. Please try again later.");
+                            setIsLoading(false);
+                            return;
+                        }
+                        setNamespacePermissions(res.data.map(el =>
+                        ({
+                            name: el.name, maxPerm: roles && roles.includes("admin") ? 7 : el.permissions
+                                .reduce((acc, curr) => (curr.username === username ? curr.permission : acc), 0)
+                        })));
+                    })
+                    .catch(err => {
+                        setSubmissionErrorMsg(`Problems while retrieving namespaces. Error message: ${getResponseError(err)}.`);
+                    })
+            ]
+            try {
+                let availableInstancesTmp;
+                if (roles && roles.includes('admin')) {
+                    const instanceData = await axios.get(`${server}/usage/instances`);
+                    availableInstancesTmp = instanceData.data.map(instance => ({
+                        "value": instance.label,
+                        "label": instance.label
+                    }));
+                } else {
+                    const instanceData = await axios.get(`${server}/usage/instances/${encodeURIComponent(username)}`);
+                    availableInstancesTmp = instanceData.data.instances_available.map(instance => ({
+                        "value": instance.label,
+                        "label": instance.label
+                    }));
                 }
-                setNamespacePermissions(res.data.map(el =>
-                ({
-                    name: el.name, maxPerm: roles && roles.includes("admin") ? 7 : el.permissions
-                        .reduce((acc, curr) => (curr.username === username ? curr.permission : acc), 0)
-                })));
+                if (availableInstancesTmp && availableInstancesTmp.length > 0) {
+                    setAvailableInstances(availableInstancesTmp);
+                    setDefaultInstance(availableInstancesTmp[0]);
+                }
+            } catch (err) {
+                setSubmissionErrorMsg(`An error occurred fetching instances. Error message: ${getResponseError(err)}.`);
                 setIsLoading(false);
-            })
-            .catch(err => {
-                setSubmissionErrorMsg(`Problems while retrieving namespaces. Error message: ${getResponseError(err)}.`);
+                return;
+            }
+            try {
+                await Promise.all(requests);
+            }
+            catch (err) {
+                console.error(err);
+            }
+            finally {
                 setIsLoading(false);
-            });
+            }
+        };
+        fetchRequiredData();
     }, [server, jwt, roles, username]);
 
     const handleInvitationSubmission = () => {
@@ -49,12 +93,41 @@ const UserInvitationForm = () => {
         if (role !== "user") {
             invitationSubmissionForm.append("roles", role);
         }
-        for (let i = 0; i < namespacePermissions.length; i++) {
-            if (namespacePermissions[i].perm == null || namespacePermissions[i].perm < 1) {
-                continue;
+        if (role !== "admin") {
+            for (let i = 0; i < namespacePermissions.length; i++) {
+                if (namespacePermissions[i].perm != null && namespacePermissions[i].perm >= 1) {
+                    invitationSubmissionForm.append("namespace_permissions",
+                        `${namespacePermissions[i].perm}@${namespacePermissions[i].name}`);
+                }
+                if (namespacePermissions[i].groups != null && namespacePermissions[i].groups.length > 0) {
+                    namespacePermissions[i].groups.forEach((label) => {
+                        invitationSubmissionForm.append("user_groups",
+                            `${label}@${namespacePermissions[i].name}`)
+                    });
+                }
             }
-            invitationSubmissionForm.append("namespace_permissions",
-                `${namespacePermissions[i].perm}@${namespacePermissions[i].name}`);
+            if (assignQuotas) {
+                if (quotas == null) {
+                    setSubmissionErrorMsg("Invalid quotas entered.");
+                    setIsSubmitting(false);
+                    return;
+                }
+                if (quotas.parallel != null) {
+                    invitationSubmissionForm.append("parallel_quota", quotas.parallel);
+                }
+                if (quotas.volume != null) {
+                    invitationSubmissionForm.append("volume_quota", quotas.volume);
+                }
+                if (quotas.disk != null) {
+                    invitationSubmissionForm.append("disk_quota", quotas.disk);
+                }
+            }
+            if (assignInstances) {
+                selectedInstancesAllowed.forEach(instance => {
+                    invitationSubmissionForm.append("labels", instance.label);
+                });
+                invitationSubmissionForm.append("default_label", defaultInstance.value);
+            }
         }
         axios
             .post(
@@ -110,20 +183,80 @@ const UserInvitationForm = () => {
                                         <select id="roleSelector" className="form-control" value={role} onChange={updateRole}>
                                             <option key="user" value="user">User</option>
                                             <option key="inviter" value="inviter">Inviter</option>
-                                            {(roles.find(role => role === "admin") !== undefined) &&
+                                            {roles && roles.includes('admin') &&
                                                 <option key="admin" value="admin">Admin</option>}
                                         </select>
                                     </div>
                                 }
-                                {role !== "admin" && <NamespacePermissionSelector
-                                    namespacePermissions={namespacePermissions}
-                                    setNamespacePermissions={setNamespacePermissions}
-                                />}
+                                {role !== "admin" &&
+                                    <>
+                                        <NamespacePermissionSelector
+                                            namespacePermissions={namespacePermissions}
+                                            setNamespacePermissions={setNamespacePermissions}
+                                            includeGroups={true}
+                                            highlight={true}
+                                        />
+                                        <div className="form-group form-check mt-3 mb-3">
+                                            <input type="checkbox" className="form-check-input" checked={assignQuotas} onChange={e => setAssignQuotas(e.target.checked)}
+                                                id="assignQuotas" />
+                                            <label className="form-check-label" htmlFor="assignQuotas">Assign Quotas?</label>
+                                        </div>
+                                        {
+                                            assignQuotas === true &&
+                                            <UserQuotaSelector
+                                                setQuotas={setQuotas} />
+                                        }
+                                        {availableInstances.length > 0 &&
+                                            <>
+                                                <div className="form-group form-check mt-3 mb-3">
+                                                    <input type="checkbox" className="form-check-input"
+                                                        checked={assignInstances !== true} onChange={e => setAssignInstances(!e.target.checked)}
+                                                        id="assignInstances" />
+                                                    <label className="form-check-label" htmlFor="assignInstances">User is allowed to use raw resource requests?</label>
+                                                </div>
+                                                {assignInstances ?
+                                                    <>
+                                                        <div className="form-group">
+                                                            <label htmlFor="instancesAllowed">
+                                                                Instances user is allowed to use
+                                                            </label>
+                                                            <Select
+                                                                id="instancesAllowed"
+                                                                value={selectedInstancesAllowed}
+                                                                isMulti={true}
+                                                                isSearchable={true}
+                                                                onChange={selected => {
+                                                                    setSelectedInstancesAllowed(selected);
+                                                                    if (defaultInstance == null ||
+                                                                        !selected.map(el => el.value).includes(selected.value)) {
+                                                                        setDefaultInstance(selected[0]);
+                                                                    }
+                                                                }}
+                                                                options={availableInstances}
+                                                            />
+                                                        </div>
+                                                        <div className="form-group">
+                                                            <label htmlFor="instancesDefault">
+                                                                Default Instance
+                                                            </label>
+                                                            <Select
+                                                                id="instancesDefault"
+                                                                isClearable={false}
+                                                                value={defaultInstance}
+                                                                isSearchable={true}
+                                                                onChange={selected => setDefaultInstance(selected)}
+                                                                options={selectedInstancesAllowed}
+                                                            />
+
+                                                        </div>
+                                                    </> : <></>}
+                                            </>}
+                                    </>}
                             </fieldset>
                             <div className="mt-3">
                                 <SubmitButton isSubmitting={isSubmitting}>
                                     Create Invitation
-                        </SubmitButton>
+                                </SubmitButton>
                             </div>
                         </form>
                     </div>
