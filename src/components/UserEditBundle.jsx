698b1f007cbbad535c8fd7f498ed3915fef8b3bc
unknown <janinahasselbring@gmail.com>
2024-08-12 12:00:56 +0200

    shorten setActiveTab

--- src/components/UserEditBundle.jsx
+++ src/components/UserEditBundle.jsx
@@ -33,21 +33,12 @@ const UserEditBundle = () => {
 
     useEffect(() => {
         const path = location.pathname;
-        if (path.includes('/usage')) {
-            setActiveTab('usage');
-        } else if (path.endsWith('/change_pass')) {
-            setActiveTab('change_pass');
-        } else if (path.endsWith('/instances')) {
-            setActiveTab('instances');
-        } else if (path.endsWith('/quotas')) {
-            setActiveTab('quotas');
-        } else if (path.endsWith('/identity_provider')) {
-            setActiveTab('identity_provider');
-        } else if (path.endsWith('/permissions')) {
-            setActiveTab('permissions');
-        } else if (path.endsWith('/licenses')) {
-            setActiveTab('licenses');
-        }
+        ['usage', 'change_pass', 'instances', 'quotas', 'identity_provider', 'permissions', 'licenses'].some((tabId) => {
+            if (path.endsWith(`/${tabId}`)) {
+                setActiveTab(tabId);
+                return true;
+            }
+        });
     }, [location]);
 
     useEffect(() => {
@@ -77,71 +68,71 @@ const UserEditBundle = () => {
     }, [server, userToEdit, setAlertMsg]);
 
     return invalidUser ?
-            <div className="alert alert-danger mt-3">
-                <p><strong>You do not have permission to view information about user: {userToEdit}.</strong></p>
-            </div> :
-                <div>
-                    <h1 className="h2">User: {userToEdit}</h1>
-                    {(isAdmin || (isInviter && username !== userToEdit)) ?
-                        <>
-                            <Tab.Container defaultActiveKey="quotas" activeKey={activeTab} onSelect={(key) => setActiveTab(key)}>
-                                <Nav className="nav-tabs">
+        <div className="alert alert-danger mt-3">
+            <p><strong>You do not have permission to view information about user: {userToEdit}.</strong></p>
+        </div> :
+        <div>
+            <h1 className="h2">User: {userToEdit}</h1>
+            {(isAdmin || (isInviter && username !== userToEdit)) ?
+                <>
+                    <Tab.Container defaultActiveKey="quotas" activeKey={activeTab} onSelect={(key) => setActiveTab(key)}>
+                        <Nav className="nav-tabs">
+                            <Nav.Item>
+                                <Nav.Link eventKey="usage" as={NavLink} to="usage">Usage</Nav.Link>
+                            </Nav.Item>
+                            {(userToEditIDP === "gams_engine") && (
+                                <Nav.Item>
+                                    <Nav.Link eventKey="change_pass" as={NavLink} to="change_pass">Change Password</Nav.Link>
+                                </Nav.Item>
+                            )}
+                            {(isAdmin) && (
+                                <Nav.Item>
+                                    <Nav.Link eventKey="licenses" as={NavLink} to="licenses">Change License</Nav.Link>
+                                </Nav.Item>
+                            )}
+                            {(serverInfo.in_kubernetes === true) && (
+                                <Nav.Item>
+                                    <Nav.Link eventKey="instances" as={NavLink} to="instances">Change Instances</Nav.Link>
+                                </Nav.Item>
+                            )}
+                            {(!userToEditIsAdmin) && (
+                                <Nav.Item>
+                                    <Nav.Link eventKey="quotas" as={NavLink} to="quotas">Change Quota</Nav.Link>
+                                </Nav.Item>
+                            )}
+                            {(userToEdit !== "admin") && (
+                                <>
                                     <Nav.Item>
-                                        <Nav.Link eventKey="usage" as={NavLink} to="usage">Usage</Nav.Link>
+                                        <Nav.Link eventKey="identity_provider" as={NavLink} to="identity_provider">Change Identity Provider</Nav.Link>
                                     </Nav.Item>
-                                    {(userToEditIDP === "gams_engine") && (
-                                        <Nav.Item>
-                                            <Nav.Link eventKey="change_pass" as={NavLink} to="change_pass">Change Password</Nav.Link>
-                                        </Nav.Item>
-                                    )}
-                                    {(isAdmin) && (
-                                        <Nav.Item>
-                                            <Nav.Link eventKey="licenses" as={NavLink} to="licenses">Change License</Nav.Link>
-                                        </Nav.Item>
-                                    )}
-                                    {(serverInfo.in_kubernetes === true) && (
-                                        <Nav.Item>
-                                            <Nav.Link eventKey="instances" as={NavLink} to="instances">Change Instances</Nav.Link>
-                                        </Nav.Item>
-                                    )}
-                                    {(!userToEditIsAdmin) && (
-                                        <Nav.Item>
-                                            <Nav.Link eventKey="quotas" as={NavLink} to="quotas">Change Quota</Nav.Link>
-                                        </Nav.Item>
-                                    )}
-                                    {(userToEdit !== "admin") && (
-                                        <>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="identity_provider" as={NavLink} to="identity_provider">Change Identity Provider</Nav.Link>
-                                            </Nav.Item>
-                                            <Nav.Item>
-                                                <Nav.Link eventKey="permissions" as={NavLink} to="permissions">Change Permissions</Nav.Link>
-                                            </Nav.Item>
-                                        </>
-                                    )}
-                                </Nav>
-                            </Tab.Container>
-                            <Tab.Content className="pt-3">
-                                <Routes>
-                                    <Route index element={<Navigate to="usage" replace />} />
-                                    <Route path="usage/*" element={<Usage />} />
-                                    <Route path="change_pass" element={<UserChangePassForm hideTitle={true} />} />
-                                    {(isAdmin) && (
-                                        <Route path="licenses" element={<LicenseUpdateForm />} />
-                                    )}
-                                    {(serverInfo.in_kubernetes === true) && (
-                                        <Route path="instances" element={<UserInstanceUpdateForm />} />
-                                    )}
-                                    <Route path="quotas" element={<UserQuotaUpdateForm />} />
-                                    <Route path="identity_provider" element={<UserUpdateIdentityProviderForm />} />
-                                    <Route path="permissions" element={<UserPermissionUpdateForm />} />
-                                </Routes>
-                            </Tab.Content>
-                        </>
-                        :
-                        <Usage />
-                    }
-                </div>
+                                    <Nav.Item>
+                                        <Nav.Link eventKey="permissions" as={NavLink} to="permissions">Change Permissions</Nav.Link>
+                                    </Nav.Item>
+                                </>
+                            )}
+                        </Nav>
+                    </Tab.Container>
+                    <Tab.Content className="pt-3">
+                        <Routes>
+                            <Route index element={<Navigate to="usage" replace />} />
+                            <Route path="usage/*" element={<Usage />} />
+                            <Route path="change_pass" element={<UserChangePassForm hideTitle={true} />} />
+                            {(isAdmin) && (
+                                <Route path="licenses" element={<LicenseUpdateForm />} />
+                            )}
+                            {(serverInfo.in_kubernetes === true) && (
+                                <Route path="instances" element={<UserInstanceUpdateForm />} />
+                            )}
+                            <Route path="quotas" element={<UserQuotaUpdateForm />} />
+                            <Route path="identity_provider" element={<UserUpdateIdentityProviderForm />} />
+                            <Route path="permissions" element={<UserPermissionUpdateForm />} />
+                        </Routes>
+                    </Tab.Content>
+                </>
+                :
+                <Usage />
+            }
+        </div>
 }
 
 export default UserEditBundle;
