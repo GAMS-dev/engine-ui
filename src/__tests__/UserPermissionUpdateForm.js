3adca2ed1c05735ec72399e1d044d77115943272
Janina Hasselbring <janinahasselbring@gmail.com>
2024-10-07 14:24:30 +0200

    fix lint

--- src/__tests__/UserPermissionUpdateForm.js
+++ src/__tests__/UserPermissionUpdateForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor, fireEvent, within } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import '@testing-library/jest-dom'
 import { AuthContext } from '../AuthContext';
