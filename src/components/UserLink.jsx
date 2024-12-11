12e83fa6cf0afe33bbb7ed8cc0cc423ac33bf190
Janina Hasselbring <janinahasselbring@gmail.com>
2024-12-11 17:09:59 +0100

    badge behind link

--- src/components/UserLink.jsx
+++ src/components/UserLink.jsx
@@ -7,11 +7,12 @@ export const UserLink = ({ user, hideMeBadge, children }) => {
     const [{ roles, username }] = useContext(AuthContext);
 
     return (username === user || roles?.length > 0 ?
-        <Link className="table-link" to={`/users/${user}`}> {user}{
-            (user === username && hideMeBadge !== true) ? <sup>
-                <span className="badge rounded-pill bg-primary ms-1">me</span>
-            </sup> : <></>
-        }{children} </Link> : <>{user}</>
-
+        <>
+            <Link className="table-link" to={`/users/${user}`}> {user}{children}</Link>{
+                (user === username && hideMeBadge !== true) ? <sup>
+                    <span className="badge rounded-pill bg-primary ms-1">me</span>
+                </sup> : <></>
+            }
+        </> : <>{user}</>
     );
 };
