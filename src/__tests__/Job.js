019536bf56166ae505b8ee146a70309fa9eb2426
Janina Hasselbring <jhasselbring@gams.com>
2025-02-06 16:55:03 +0100

    fix lint

--- src/__tests__/Job.js
+++ src/__tests__/Job.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor, screen, fireEvent } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom'
 import { AuthContext } from '../AuthContext';
