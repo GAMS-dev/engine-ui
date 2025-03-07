34826bc84135c94d011086148ef85d763f87d387
Janina Hasselbring <jhasselbring@gams.com>
2025-03-07 17:17:19 +0100

    add missing import

--- src/__tests__/InexJSONSelector.js
+++ src/__tests__/InexJSONSelector.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 
 import InexJSONSelector from '../components/InexJSONSelector'
+import { suppressActWarnings } from './utils/testUtils';
 
 jest.mock('axios');
 
