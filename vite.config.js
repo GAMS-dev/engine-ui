4c015db2ceedd97b550b90f1aa82111f4689aff8
Janina Hasselbring <jhasselbring@gams.com>
2026-03-02 14:28:13 +0100

    add proxy for v2

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
     server: {
         port: 3000,
         proxy: {
+            '/api/v2': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+            },
             '/api': {
                 target: 'http://localhost:80',
                 changeOrigin: true,
