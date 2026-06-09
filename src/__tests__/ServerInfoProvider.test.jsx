eefb2e9f02175de482e3453ff6dfc75c80861cb6
Frederik Proske <fproske@gams.com>
2026-06-09 14:45:44 +0200

    Fix `ServerInfoProvider` test
    `use_brokerv2` now only `true` if `hostname` is SaaS domain

--- src/__tests__/ServerInfoProvider.test.jsx
+++ src/__tests__/ServerInfoProvider.test.jsx
@@ -57,13 +57,53 @@ describe('ServerInfoProvider', () => {
     });
 
     expect(await screen.findByTestId('version')).toHaveTextContent('x.x.x');
-    expect(screen.getByTestId('brokerv2')).toHaveTextContent('true');
+    expect(screen.getByTestId('brokerv2')).toHaveTextContent('false'); // since is_saas if false
     expect(screen.getByTestId('saas')).toHaveTextContent('false'); // since the hostname is testserver and not engine.gams.com
+
+    const cachedData = JSON.parse(localStorage.getItem('serverInfo'));
+    expect(cachedData.version).toBe('x.x.x');
+    expect(cachedData.use_brokerv2).toBe(false);
+    expect(cachedData.timestamp).toBeDefined();
+  });
+
+  it('fetches server info when no valid cache exists (using SaaS domain)', async () => {
+    vi.stubGlobal('location', {
+      ...window.location,
+      hostname: 'engine.gams.com',
+    });
+    expect(window.location.hostname).toBe('engine.gams.com');
+    axios.get.mockImplementation((url) => {
+      if (url.includes('/version'))
+        return Promise.resolve({ data: { version: 'x.x.x' } });
+      if (url.includes('/configuration'))
+        return Promise.resolve({ data: { use_brokerv2: true } });
+      return Promise.reject(new Error('not found'));
+    });
+
+    render(
+      <ServerInfoProvider>
+        <DummyConsumer />
+      </ServerInfoProvider>,
+    );
+
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledWith(
+        expect.stringContaining('/version'),
+      );
+      expect(axios.get).toHaveBeenCalledWith(
+        expect.stringContaining('/configuration'),
+      );
+    });
+
+    expect(await screen.findByTestId('version')).toHaveTextContent('x.x.x');
+    expect(screen.getByTestId('saas')).toHaveTextContent('true');
+    expect(screen.getByTestId('brokerv2')).toHaveTextContent('true');
 
     const cachedData = JSON.parse(localStorage.getItem('serverInfo'));
     expect(cachedData.version).toBe('x.x.x');
     expect(cachedData.use_brokerv2).toBe(true);
     expect(cachedData.timestamp).toBeDefined();
+    vi.unstubAllGlobals();
   });
 
   it('does not fetch data if valid, up-to-date cached data is present', async () => {
